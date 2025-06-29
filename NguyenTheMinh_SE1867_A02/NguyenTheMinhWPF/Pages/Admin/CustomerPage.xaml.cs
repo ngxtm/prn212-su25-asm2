ee9afb205a19b4d0ae7c8f8063e184d3967c44a1
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-06-29 16:28:06 +0700

    Add UI Customer Page

--- NguyenTheMinh_SE1867_A02/NguyenTheMinhWPF/Pages/Admin/CustomerPage.xaml.cs
+++ NguyenTheMinh_SE1867_A02/NguyenTheMinhWPF/Pages/Admin/CustomerPage.xaml.cs
@@ -24,5 +24,30 @@ namespace NguyenTheMinhWPF.Pages.Admin
         {
             InitializeComponent();
         }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dgCustomer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
