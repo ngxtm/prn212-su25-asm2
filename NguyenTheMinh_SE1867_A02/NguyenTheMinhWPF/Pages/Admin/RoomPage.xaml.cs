86b9c6fbf31b0947b8e0538dceb654acc3f6012c
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-07-05 00:29:43 +0700

    Add search and get all room information
    Frontend logic added

--- NguyenTheMinh_SE1867_A02/NguyenTheMinhWPF/Pages/Admin/RoomPage.xaml.cs
+++ NguyenTheMinh_SE1867_A02/NguyenTheMinhWPF/Pages/Admin/RoomPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL.Services;
+using DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,90 @@ namespace NguyenTheMinhWPF.Pages.Admin
     /// </summary>
     public partial class RoomPage : Page
     {
+        private readonly RoomInformationService _roomInformationService;
+
         public RoomPage()
         {
             InitializeComponent();
+            this._roomInformationService = new RoomInformationService();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var rooms = _roomInformationService.GetAllRoomInformations();
+                if (rooms != null)
+                {
+                    dgRoom.ItemsSource = rooms;
+                }
+                else
+                {
+                    MessageBox.Show("Không có dữ liệu phòng!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi tải dữ liệu: {ex.Message}");
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtRoomNumber.Text = "";
+            txtRoomDetailDescription.Text = "";
+            txtMaxCapacity = null;
+            cbRoomType.SelectedIndex = -1;
+            cbStatus.SelectedIndex = -1;
+            txtPrice.Text = "";
+        }
+
+        private void AutoFillInputFields(RoomInformation room)
+        {
+            txtRoomNumber.Text = room.RoomNumber ?? string.Empty;
+            txtRoomDetailDescription.Text = room.RoomDetailDescription ?? string.Empty;
+            txtMaxCapacity.Text = room.RoomMaxCapacity?.ToString() ?? string.Empty;
+            cbRoomType.SelectedValue = room.RoomType?.RoomTypeId;
+            cbStatus.SelectedValue = room.RoomStatus;
+            txtPrice.Text = room.RoomPricePerDay?.ToString("F2") ?? string.Empty;
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            dgRoom.ItemsSource = _roomInformationService.SearchRoom(txtSearch.Text ?? string.Empty);
+            Page_Loaded(sender, e);
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dgRoom_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dgRoom.SelectedItem is RoomInformation selectdRoom)
+            {
+                AutoFillInputFields(selectdRoom);
+            }
+            else
+            {
+                ClearForm();
+            }
         }
     }
 }
